bf7a7b069cf565dbb37ba18579268112536e365f
Drew <113324775+Derwplayz@users.noreply.github.com>
2025-11-10 05:53:02 -0500

    Auto-update job ID: 6439dc4e-c0b3-454b-9678-6d5b77bba07c

--- server_joiner.lua
+++ server_joiner.lua
@@ -10,7 +10,7 @@ print("🆔 Server Job ID: " .. game.JobId)
 print("")
 
 -- Single job ID that will be updated by Flask server
-local jobId = "eccb8473-aca4-42dd-a2c5-22765a25ae09"
+local jobId = "6439dc4e-c0b3-454b-9678-6d5b77bba07c"
 
 -- Flag to track if we're currently teleporting
 local isTeleporting = false
