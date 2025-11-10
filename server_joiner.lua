2f22e42052677b00e88ec2e1101f8c6e97730c1b
Drew <113324775+Derwplayz@users.noreply.github.com>
2025-11-10 02:47:51 -0500

    Auto-update job ID: 65e5dddc-cc62-4143-8d28-ce041fa52302

--- server_joiner.lua
+++ server_joiner.lua
@@ -1,5 +1,5 @@
 -- Combined Animal Scanner + Auto Server Joiner
-local jobId = "b45805f3-96d0-4ae7-a0ba-1ae9910dd513"
+local jobId = "65e5dddc-cc62-4143-8d28-ce041fa52302"
 
 -- Your Lua script content here
 print("Server Job ID: " .. jobId)
